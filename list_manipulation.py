68111aef1050cd3ffd1f92cceeb3007f88f0b5cc
johnwoates <77648852+johnwoates@users.noreply.github.com>
2021-01-31 21:08:16 -0800

    Updated content

---
+++ list_manipulation.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+
+johnwoates
+CPSC 223P-01
+Thu Jan 28, 2021
+joates@fullerton.edu
+
+"""
+
+import lists
+
+sports_teams = [lists.football_teams, lists.baseball_teams, lists.basketball_teams]
+
+# Print out all the school lunches on the menu, but substitute Bratwurst wherever you see hot dogs
+# Use list comprehension. Just print the list directly so the output will
+# include the brackets and quotations (['item 1', item 2' ... and so on])
+
+
+# Use zip to iterate over two lists at the same time
+# Print out questions and answers in a loop
+# Format them: "What is your <question>? My <question> is <answer>."
+
+
+# Manipulate the nested lists of sports teams to print all teams from New York
+# and all teams from Los Angeles.  Just print the lists directly so the output will
+# include the brackets and quotations (['team 1', team 2' ... and so on])
